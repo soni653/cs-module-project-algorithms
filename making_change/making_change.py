9d2a79e2a4fbaa4fc0d01c38d93fe93305f81c35
soni653 <dave.family653@outlook.com>
2020-09-13 14:32:18 -0400

    Fix making _change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,34 +3,22 @@
 import sys
 
 def making_change(amount, denominations):
-  if cache is None:
-    cache = {}
-
-  if amount == 0 and amount < 5:
-    return 1
-
-  if amount < 0:
-    return 0
-
-  if cache and cache[amount]:
+    # intializes list with 0 of sze ammoutn
+    cache = [0 for _ in range(amount + 1)]
+    # one way to get zero coins
+    cache[0] = 1
+    # for each denominatio of coin
+    for coin_value in denominations:
+        # consider each coin value at a time
+        for amt in range(1, amount + 1):
+            diff_amount = amt - coin_value
+            if diff_amount >= 0:
+                # how many ways do we know how to make diff_amount (so far)?
+                ways_to_make_diff_amount = cache[diff_amount]
+                # we know we can make amt by adding the current coin value to any of the ways we make diff_amount,
+                # in addition to all the ways we've found to make amt with other coin values
+                cache[amt] = cache[amt] + ways_to_make_diff_amount
     return cache[amount]
-
-
-  for coin in denominations:
-    cache[amount] = making_change(amount-coin, denominations)
-
-  return cache[amount]
-
-
-
-
-
-    # return num_ways_to_make_change
-    # for i in range(0, len(denominations)):
-    #     for j in range(denominations[i], amount + 1):
-    #         table[j] += table[j - denominations[i]]
-
-
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
